a0f39e83ea45db09023acb56c99c0fbdcbdf282f
chenwei <cw.itcas.com>
2019-06-16 21:25:38 +0800

    把本地工程添加到Git

---
+++ mavenGaoji/git-text/src/com/itheima/gitTest.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("你好金同学");
+        System.out.println("你好金同学");
+        System.out.println("你好金同学");
+        System.out.println("你好金同学");
+        System.out.println("你好金同学");
+    }
+}
